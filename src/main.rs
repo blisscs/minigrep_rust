5ee1b40eff8dee5298e16a3b7faeaf19a40fefc6
Suracheth Chawla <me@suracheth.com>
2024-01-23 22:26:33 +0700

    Remove debugging and ignore the unused variables

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let _query = &args[1];
+    let _file_path = &args[2];
 }
