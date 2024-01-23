73947be91b243ec7721ef4d310b84b4b6f88d798
Suracheth Chawla <me@suracheth.com>
2024-01-23 23:12:08 +0700

    Further develop by seperating into lib.rs and main.rs files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
-}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    (query, file_path)
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
