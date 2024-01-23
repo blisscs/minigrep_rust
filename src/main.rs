83b21bf31d4d906d9c04579865c589441982baf7
Suracheth Chawla <me@suracheth.com>
2024-01-23 22:36:58 +0700

    Refactor into parse_config function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_config(&args);
+}
 
-    let _query = &args[1];
-    let _file_path = &args[2];
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
